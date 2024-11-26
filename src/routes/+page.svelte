d8325cbc02ad91730e4153e7a31584e5415416fa
Panda <84963742+0Pandab@users.noreply.github.com>
2024-11-26 11:21:58 +0100

    Margin angepasst

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 	class="flex-container box-content flex h-[450px] w-[800px] flex-col rounded-l-3xl rounded-br-[175px] rounded-tr-3xl border-4
 border-red-600 bg-white p-4"
 >
-	<div class="mb-5 ml-5 mt-5">
+	<div class="ml-5 mt-5">
 		<div class="flex h-[110px] w-[550px] items-center justify-evenly border-4 border-blue-500">
 			<div class="  justify-left flex h-[90px] w-[140px] flex-col gap-2 border-2 border-green-400">
 				<div class=" h-[30px] w-[80px] border-2 border-purple-400">DAY</div>
@@ -31,10 +31,10 @@ border-red-600 bg-white p-4"
 			</div>
 		</div>
 
-		<div class="mt-2 flex items-center space-x-4">
-			<hr class="w-[625px] border-gray-300 before:border-t-2" />
+		<div class="flex items-center space-x-4">
+			<hr class="mt-[-20px] w-[625px] border-gray-300 before:border-t-2" />
 			<button
-				class="flex h-20 w-20 items-center justify-center rounded-full bg-indigo-600 text-white"
+				class="mt-[-20px] flex h-20 w-20 items-center justify-center rounded-full bg-indigo-600 text-white"
 				aria-label="calculate_age"
 			>
 				<iconify-icon icon="iconoir:arrow-down" class="text-5xl"></iconify-icon>
